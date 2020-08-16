44cb2657e349945d6255f98862b83eade03b231e
kyzdra <kyzdra@yahoo.fr>
2020-08-16 15:26:50 +0200

    Slight stern optimization: avoid recomputing the l first rows after collision

--- src/stern/mod.rs
+++ src/stern/mod.rs
@@ -21,60 +21,73 @@ pub fn stern(inst: &Instance, p: usize, l: usize, max_tries: Option<usize>) -> O
     let mut h_sf = Mat::zero(Rc::clone(&f2), n - k, n);
     let mut pi = Perm::identity(n);
     let mut us = ColVec::zero(Rc::clone(&f2), n - k);
-    let e1_selection = Rc::new(stern_list_weighted_vectors(k1, p));
-    let e2_selection = if k1 == k2 {
-        Rc::clone(&e1_selection)
+    let e1_supports = Rc::new(stern_list_weighted_vectors(k1, p));
+    let e2_supports = if k1 == k2 {
+        Rc::clone(&e1_supports)
     } else {
         Rc::new(stern_list_weighted_vectors(k2, p))
     };
-    let e1_len = e1_selection.len();
-    let e2_len = e2_selection.len();
-    // let e1_len = num_integer::binomial(k1, p);
-    // let e2_len = num_integer::binomial(k2, p);
+    let e1_len = e1_supports.len();
+    let e2_len = e2_supports.len();
     let mut l1 = vec![ColVec::zero(Rc::clone(&f2), l); e1_len];
     let mut q2e2_us = ColVec::zero(Rc::clone(&f2), l);
-    let mut qe_us = ColVec::zero(Rc::clone(&f2), n - k);
+    let mut qe_us = ColVec::zero(Rc::clone(&f2), n - k); // l first coordinates will be left null
     loop {
+        // Randomly select an information set and put h in standard form
         h.parity_check_random_standard_form(&mut u, &mut h_sf, &mut pi);
+
+        // Compute the altered syndrome
         us.mul(&u, s);
+
+        // Denoting X the set of the first k1 columns of h restricted to their first l rows,
+        // compute the list l1 of p-subset sums of X.
         let q1 = SubMat::new(&h_sf, 0, l, 0, k1);
         for i in 0..e1_len {
             for j in 0..l {
                 l1[i][j] = f2.zero();
                 for m in 0..p {
-                    f2.add_assign(&mut l1[i][j], &q1[(j, e1_selection[i].support()[m])]);
+                    f2.add_assign(&mut l1[i][j], &q1[(j, e1_supports[i].support()[m])]);
                 }
             }
         }
+
+        // Denoting Y the set of columns k1, k1 + 1, ..., k - 1 of h restricted to their first l rows,
+        // for each p-subset sum of Y:
+        // - add to it (the first l rows of) the altered syndrome,
+        // - look for a collision q1e1 = q2e2 + us with list l1 such that the complete sum (on all rows)
+        //   q1e1 + q2e2 + us has weight at most 2 * p.
         let q2 = SubMat::new(&h_sf, 0, l, k1, k);
         for i in 0..e2_len {
             for j in 0..l {
                 q2e2_us[j] = us[j];
                 for m in 0..p {
-                    f2.add_assign(&mut q2e2_us[j], &q2[(j, e2_selection[i].support()[m])]);
+                    f2.add_assign(&mut q2e2_us[j], &q2[(j, e2_supports[i].support()[m])]);
                 }
             }
             for j in 0..e1_len {
                 if l1[j] == q2e2_us {
-                    for m in 0..us.rows() {
+                    let mut weight = 0;
+                    for m in l..n - k {
                         qe_us[m] = us[m];
-                    }
-                    for &col in e1_selection[j].support() {
-                        for i in 0..us.rows() {
-                            f2.add_assign(&mut qe_us[i], &h_sf[(i, col)]);
+                        for &col in e1_supports[j].support() {
+                            f2.add_assign(&mut qe_us[m], &h_sf[(m, col)]);
+                        }
+                        for &col in e2_supports[i].support() {
+                            f2.add_assign(&mut qe_us[m], &h_sf[(m, k1 + col)]);
+                        }
+                        if qe_us[m] == f2.one() {
+                            weight += 1;
+                            if weight > w - 2 * p {
+                                break;
+                            }
                         }
                     }
-                    for &col in e2_selection[i].support() {
-                        for i in 0..us.rows() {
-                            f2.add_assign(&mut qe_us[i], &h_sf[(i, k1 + col)]);
-                        }
-                    }
-                    if qe_us.weight() <= w - 2 * p {
+                    if weight <= w - 2 * p {
                         let mut e = ColVec::zero(Rc::clone(&f2), n);
-                        for &col in e1_selection[j].support() {
+                        for &col in e1_supports[j].support() {
                             e[col] = f2.one();
                         }
-                        for &col in e2_selection[i].support() {
+                        for &col in e2_supports[i].support() {
                             e[k1 + col] = f2.one();
                         }
                         for (i, x) in qe_us.data().iter().enumerate() {
